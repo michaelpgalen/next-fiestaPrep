2a100b481212273cd91e17a4135c4e2311c0c763
michaelpgalen <52191377+michaelpgalen@users.noreply.github.com>
2022-08-16 13:06:16 -0700

    Delete extra config folder

--- config/index.js
+++
@@ -1,7 +0,0 @@
-// made this config folder and this file to check if we're in dev mode or not.
-// if in dev mode, then we are fetching from localhost:3000
-// when deployed, we'll need to fetch from the url of the data, not localhost:3000
-
-const dev = process.env.NODE_ENV !== 'production'
-
-export const server = dev ? 'http://localhost:3000' : 'https://yourwebsite.com'
